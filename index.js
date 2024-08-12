e6c3e0905d40c9095dab0c3668dca50de01a9ece
alexPietro <alexpieetro@gmail.com>
2024-08-12 11:01:21 -0300

    terminado

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import hand from 'express-handlebars'
+import fs from 'fs'
+import { title } from 'process'
+const log = console.log
+var id = 0;
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const hbs = hand.create({partialsDir: ["views/partials"]})
-
+const jsonData = JSON.parse(fs.readFileSync(path.join(__dirname, './public', 'data', 'data.json')))
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'Projeto/public')));
+app.use('/public', express.static(path.join(__dirname, './public')));
 
 app.engine('handlebars', hbs.engine)
+
 app.set("view engine", "handlebars")
 
 app.get('/', (req, res) =>{
     res.render('home')
 })
 
-app.get('/oda', (req, res) =>{
-
+app.get('/noticia/:id', (req, res) =>{
+    id = parseInt(req.params.id, 10);
+    var item = jsonData.noticias.find(item => item.id === id)
+    res.render('page', {
+        layout: 'noticia',
+        title: item ? item.title: 'Titulo não encontrado',
+        noticiaTitulo: item ? item.noticiaTitulo: 'Titulo não encontrado',
+        info: item ? item.info: 'Conteudo não encontrado',
+        sections: item ? item.sections : []
+    })
 })
 
-app.listen(3000, () => console.log('App rodando na porta localhost:3000'));
+app.listen(3000, () => log('App rodando na porta localhost:3000'))
